fdd4ed4b530bfbd409b368955000544c2d632b46
Patryk Szydłowski <thewebdevmachine@gmail.com>
2024-04-25 16:03:56 +0200

    fix(builders): adjust plugin names for loader paths

--- nix/packages/neovim/builders/neovim-loader.nix
+++ nix/packages/neovim/builders/neovim-loader.nix
@@ -12,6 +12,8 @@ let
       lib.strings.getName
       (lib.strings.removePrefix "vimplugin-")
       (lib.strings.removePrefix "lua5.1-")
+      (lib.strings.removeSuffix "-nvim")
+      (builtins.replaceStrings [ "_" ] [ "-" ])
     ];
   luaPluginPathEntry = plugin: ''["${luaPluginName plugin}"] = "${plugin.outPath}"'';
   luaPluginPathEntries = lib.strings.concatMapStringsSep ",\n " luaPluginPathEntry;
