da48033bd199847b791f8f73dfdc91300d4f8c9e
Patryk Szydłowski <thewebdevmachine@gmail.com>
2024-04-25 08:43:01 +0200

    fix(plugins): faster code completion loading

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,6 @@
 return require("loader").plugin({
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/cmp-path" },
